3a9279206467ba0187e0f34920993543dc3ac355
bidetaggle <bidetaggle@gmail.com>
2024-11-24 22:11:24 +0000

    fix spacing

--- website/src/components/HomePage/index.tsx
+++ website/src/components/HomePage/index.tsx
@@ -8,13 +8,15 @@ import Description from "./Description";
 import EducationProgram from "./EducationProgram";
 import ResearchProgram from "./ResearchProgram";
 
+const GAP = {base: 'sm', sm: 'xl'}
+
 export default function HomePage() {  
   return <>
     <AppShell>
       <LandingScreen />
-      <AppShell.Main p={{base: 'sm', xs: 'xl'}}>
-        <Flex gap={{base: 'xs', lg: 'xl'}} wrap="wrap" justify="center">
-          <Flex flex={1.1} gap={{base: 'xs', lg: 'xl'}} direction="column">
+      <AppShell.Main p={GAP}>
+        <Flex gap={GAP} wrap="wrap" justify="center">
+          <Flex flex={1.1} gap={GAP} direction="column">
             <Box>
               <Description />
             </Box>
