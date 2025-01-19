315e04501db3871e9fe225bdac36a76510e24050
bidetaggle <bidetaggle@gmail.com>
2025-01-19 22:42:10 +0000

    split members and adherents

--- website/src/app/community/Members/index.tsx
+++ website/src/app/community/Members/index.tsx
@@ -1,4 +1,4 @@
-import { Table, Stack } from "@mantine/core";
+import { Table, Stack, Badge } from "@mantine/core";
 import { members, candidates } from "@/data";
 import RowMember from "./RowMember";
 import { IconUsersGroup } from "@tabler/icons-react";
@@ -7,6 +7,9 @@ import type { LayoutCardOptProps } from "@/components/Layout";
 import RowCandidate from "./RowCandidate";
 
 export default function Members(props: LayoutCardOptProps) {
+  const registeredMembers = members.filter(member => member.txRegistration);
+  const unregisteredMembers = members.filter(member => !member.txRegistration);
+
   return <Card 
     {...props}
     icon={<IconUsersGroup />}
@@ -23,7 +26,31 @@ export default function Members(props: LayoutCardOptProps) {
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>
-          {members.map((member, i) => (
+          {registeredMembers.map((member, i) => (
+            <RowMember
+              key={i}
+              occupation={member.occupation}
+              address={member.address}
+              txRegistration={member.txRegistration}
+              certification={member.certification}
+              identity={member.identity}
+            />
+          ))}
+        </Table.Tbody>
+      </Table>
+
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th w={300}>
+              Adherents <Badge variant="filled" color="pink">6 slots remaining</Badge>
+            </Table.Th>
+            <Table.Th w={300}></Table.Th>
+            <Table.Th w={300} visibleFrom="sm"></Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {unregisteredMembers.map((member, i) => (
             <RowMember
               key={i}
               occupation={member.occupation}
